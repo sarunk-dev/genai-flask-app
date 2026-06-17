2fec0ed924c327f999ab2a997c95ca16f3b672e8
Arun Kumar <seerlaarun.dev@gmail.com>
2026-06-17 06:38:21 -0400

    fixed parsed error

--- llm_test.py
+++ llm_test.py
@@ -5,9 +5,16 @@ def call_all_models(system_prompt, user_prompt):
     granite_result = granite_response(system_prompt, user_prompt)
     mistral_result = mistral_response(system_prompt, user_prompt)
 
-    print("Llama Response:\n", llama_result.content)
-    print("\nGranite Response:\n", granite_result.content)
-    print("\nMistral Response:\n", mistral_result.content)
+        # Debug: show complete returned object
+    print("Llama Raw Output:")
+    print(llama_result)
+    print("..................................")
+    print("Llama Response:\n", llama_result["response"])
+    print("\nGranite Response:\n", granite_result["response"])
+    print("\nMistral Response:\n", mistral_result["response"])
 
-# Example call to test all modelsss
-call_all_models("You are a helpful assistant who provides concise and accurate answers", "What is the capital of Canada? Tell me a cool fact about it as well")
+call_all_models(
+    "You are a helpful assistant who provides concise and accurate answers",
+    "What is the capital of Canada? Tell me a cool fact about it as well"
+)
+
